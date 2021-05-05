1d76d9ae886a002749eb8cebd1c449b25f2a336c
Cryogenproxy <cryogenproxy@protonmail.com>
2021-05-04 22:05:16 -0700

    Bot

--- Key.py
+++ Key.py
@@ -1,1 +1,10 @@
 import discord
+from discord.ext import commands
+
+client = commands.Bots(command_prefix = '$')
+
+@client.event
+async def on_ready():
+    print('bot is online')
+
+client.run(TOKEN)
