b16391a88c91e4df407ad4a92df2d99d49ed56e6
Cryogenproxy <cryogenproxy@protonmail.com>
2021-05-04 23:11:48 -0700

    Eatupdate

--- Key.py
+++ Key.py
@@ -1,7 +1,7 @@
 import discord
 import os 
 from discord.ext import commands
-import asyncio
+
 
 TOKEN = os.environ['TOKEN']
 client = discord.Client()
@@ -11,37 +11,7 @@ client = commands.Bot(command_prefix = '$')
 async def on_ready():
     print('bot is online')
 
-@client.command(pass_context=True)
-async def help(ctx):
-    author = ctx.message.author
-
-    embed = discord.Embed(
-        colour = discord.Colour.red()
-    )
-
-    embed.set_author(name="Help: ")
-    embed.add_field(name="help", value="Help Command", inline=False)
-
-    await client.send_message(author, embed=embed)
-    await client.say("Message sent to your DM's :thumbsup:")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$ping'):
-        await message.channel.send('Pong Bitch you been hacked!')
-
-@client.event
-
-async def on_message(message):
-    if message.content == 'skynet':
-        await message.channel.send('Execute Y/N?')
-
-@asyncio.coroutine
-async def delete_messages(messages):
-    if message.content == 'Y':
-        await message.delete_messages()
-
+@client.command()
+async def ping(ctx):
+    await ctx.send("pong You been Hacked Bitch")
 client.run(TOKEN)
