37ca6f47708f56e37f92315456a012eb79ef2fc4
Cryogenproxy <cryogenproxy@protonmail.com>
2021-05-04 23:01:54 -0700

    Update

--- Key.py
+++ Key.py
@@ -1,13 +1,29 @@
 import discord
 import os 
+import discord.ext import commands
 import asyncio
 
 TOKEN = os.environ['TOKEN']
 client = discord.Client()
+client = commands.Bot(comma_prefix = '$')
 
 @client.event
 async def on_ready():
     print('bot is online')
+
+@client.command(pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
+
+    embed = discord.Embed(
+        colour = discord.Colour.red()
+    )
+
+    embed.set_author(name="Help: ")
+    embed.add_field(name="help", value="Help Command", inline=False)
+
+    await client.send_message(author, embed=embed)
+    await client.say("Message sent to your DM's :thumbsup:")
 
 @client.event
 async def on_message(message):
