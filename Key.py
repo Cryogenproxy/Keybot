c501b94a22950ea5e8c2318fe1af03bbd68d41b8
Cryogenproxy <cryogenproxy@protonmail.com>
2021-05-04 22:27:38 -0700

    bot2

--- Key.py
+++ Key.py
@@ -1,10 +1,18 @@
 import discord
-from discord.ext import commands
+import os 
 
-client = commands.Bots(command_prefix = '$')
+TOKEN = os.environ['TOKEN']
+client = discord.Client()
 
 @client.event
 async def on_ready():
     print('bot is online')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$ping'):
+        await message.channel.send('Pong Bitch you been hacked!')
 client.run(TOKEN)
