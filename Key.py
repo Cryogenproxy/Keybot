78ae81d2824ed84bf29da5a1bdbdb8d7250578a5
CryogenProxy <>
2021-05-06 08:00:58 +0000

    Version

--- Key.py
+++ Key.py
@@ -22,7 +22,7 @@ async def on_disconnect():
 
 @client.command(name='Version')
 async def Version(context):
-  
+    await ctx.message.delete()
     versionEmbed = discord.Embed(title="Current Version", description="Keybot is in Alpha 1.0", color=0x10a13)
 
     versionEmbed.add_field(name="Keybot Version", value="vA.1.0.0", inline=False)
