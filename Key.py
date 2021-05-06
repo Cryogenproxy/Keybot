96bc4cd76717c76f05a7145d33fd3dec11eb8ab3
Cryogenproxy <cryogenproxy@protonmail.com>
2021-05-05 23:40:57 -0700

    Join message

--- Key.py
+++ Key.py
@@ -13,6 +13,16 @@ client = commands.Bot(command_prefix = '$')
 async def on_ready():
     print('bot is online')
 
+@client.event
+async def on_disconnect():
+    annoucment_channel = client.get_channel(697670402863792138)
+    await annoucment_channel.send("Discord Bot Update, In Progress I will be Offline for a bit.")
+
+@client.command
+async def joined(ctx, *, member: discord.Member):
+    await ctx.send('Welcome to the Server please read our #rules, and if you have any questions contact, a Moderator. We Welcome you to our Collective of Anonymous.')
+    await ctx.send('{0} joined on {0.joined_at}'.format(member))
+    
 @client.command()
 async def ping(ctx):
     await ctx.message.delete()
@@ -27,13 +37,6 @@ async def Help(ctx):
     $-Ping for Testing Bot Response
     $-Purge Deletes groups of Messages
     """)
-
-@client.event
-async def on_disconnect():
-    annoucment_channel = client.get_channel(697670402863792138)
-    await annoucment_channel.send("Discord Bot Update, In Progress I will be Offline for a bit.")
-
-
 
 keep_alive()
 client.run(TOKEN)
