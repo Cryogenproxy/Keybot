df3465892f4a4f58774a206ff2d3d0c82d47afd7
Cryogenproxy <cryogenproxy@protonmail.com>
2021-05-06 00:08:45 -0700

    Version Embed

--- Key.py
+++ Key.py
@@ -17,12 +17,20 @@ async def on_ready():
 async def on_disconnect():
     annoucment_channel = client.get_channel(697670402863792138)
     await annoucment_channel.send("Discord Bot Update, In Progress I will be Offline for a bit.")
+
 @client.event
 async def on_member_join():
     await message.send.channel('Welcome Please read our rules, in #rules')
 
 @client.command
-async def whois(ctx, *, member: discord.Member):
+async def Version():
+    versionEmbed = discord.Embed(title="Current Version", description="Keybot is in Alpha 1.0", color=2A2D32)
+    versionEmbed.add_field(name='Keybot Version', value='vA.1.0.0', inline=True)
+    versionEmbed.add_field(name='Release Date:', 'May 6th, 2021', inline=True)
+    await ctx.send(embed=VersionEmbed)
+    
+@client.command
+async def joined(ctx, *, member: discord.Member):
     await ctx.send('This user Account joined at this Time')
     await ctx.send('{0} joined on {0.joined_at}'.format(member))
 
